e88c1485dc37ea6c76941b56d9d5ff67af078663
jc7441 <jc7441@nyu.edu>
2016-09-28 10:59:44 -0400

    this is the revised version of the exercise following all the instructions!

--- JoannaChen_Ex_5/sketch.js
+++ JoannaChen_Ex_5/sketch.js
@@ -1,4 +1,4 @@
-var peach, nyu;
+var peach, nyu; //colors
 var stepX = 0;
 var stepY = 0;
 
@@ -11,70 +11,98 @@ function setup() {
 
 function draw() {
   background(255);
+  eyeColor = color(41, 20, 3);
+  bodyColor = color(209, 113, 117);
+  bodyColor2 = color(250, 246, 129);
 
-  noStroke();
+//when the mouse is pressed then the eye color of BOTH monster will be randomly chosen colors
+  if (mouseIsPressed) {
+    eyeColor = color(random(0, 255), random(0, 255), random(0, 255));
+  }
 
-  //upper body
-  fill(25, 255, 45);
-  rect(185, 145, 30, 60);
+//when the lowercase x key is pressed then the belly of my bigger monster will be randomly chosen colors
+  if (keyIsPressed) {
+    (keyCode == 'x')
+    bodyColor = color(random(0, 255), random(0, 255), random(0, 255));
+  }
 
-  //head
-  fill(255, 123, 0);
-  ellipse(200, 100, 100, 100);
+nazMonster(-100 + stepX, 300 + stepY, peach); //calls monster
+nazMonster(300, 300, nyu); //calls monster
 
-  //ears
-  fill(0);
-  triangle(165, 65, 165, 20, 185, 55);
-  triangle(235, 65, 235, 20, 215, 55);
-
-  //leg
-  fill(0, 203, 255);
-  rect(195, 205, 10, 40);
-
-  //foot
-  triangle(195, 245, 205, 245, 200, 260);
-  triangle(195, 245, 197, 252, 185, 260);
-  triangle(205, 245, 203, 252, 215, 260);
-
-  /*trial body looks like table
-  //lower body
-  fill(0, 203, 255);
-  ellipse(200, 200, 300, 50);
-  
-  //legs
-  rect(50, 197, 15, 70);
-  rect(335, 197, 15, 70);
-  rect(150, 222, 20, 45);
-  rect(240, 222, 20, 45);
-  */
-
-  //arms
-  quad(140, 150, 185, 155, 185, 160, 125, 165);
-  quad(260, 150, 215, 155, 215, 160, 275, 165);
-  //rect(215, 150, 60, 10);
-
-  //mouth
-  fill(255, 50, 15);
-  quad(175, 135, 180, 105, 220, 90, 230, 120);
-
-  //teeth
-  fill(255);
-  triangle(180, 105, 182, 120, 185, 102);
-  triangle(190, 99, 194, 118, 195, 98);
-  triangle(200, 96, 206, 114, 208, 94);
-  triangle(212, 93, 214, 112, 220, 90);
-
-  //eye slits
-  fill(0);
-  triangle(165, 75, 190, 85, 180, 85);
-  triangle(225, 75, 200, 85, 210, 85);
-
-  monster(-100 + stepX, 300 + stepY, peach); //calls monster
-  monster(300, 300, nyu); //calls monster
+myMonster(125, 10, .9, eyeColor, bodyColor);
+myMonster(-100, 0, .3, eyeColor, bodyColor2);
 
 }
 
-function monster(locX, locY, headColor) {
+
+function myMonster(newX, newY, newScale, eyeColor, bodyColor) {
+  scale(newScale);
+  translate(newX, newY);
+
+  stroke(100, 0, 0);
+  // legs
+  fill(209, 113, 117);
+  quad(100, 575, 125, 454, 160, 478, 170, 575); // left
+  quad(220, 575, 235, 478, 275, 454, 290, 575); // right
+  // body
+  fill(41, 20, 3);
+  ellipse(200, 360, 220, 250); // shell
+  // abdomen
+  fill(bodyColor);
+  ellipse(200, 360, 150, 180);
+  noFill();
+  arc(200, 300, 109, 20, 0, PI);
+  arc(200, 330, 139, 20, 0, PI);
+  arc(200, 360, 147, 20, 0, PI);
+  arc(200, 390, 139, 20, 0, PI);
+  arc(200, 420, 109, 20, 0, PI);
+  fill(209, 113, 117);
+  ellipse(200, 200, 140, 170); // head
+  noStroke();
+  fill(157, 67, 67);
+  ellipse(194, 208, 8, 8);
+  ellipse(205, 208, 8, 8);
+  fill(122, 67, 67);
+  rect(160, 162, 34, 3); // left eyebrow
+  rect(205, 162, 34, 3); // right eyebrow
+  fill(255);
+  ellipse(180, 180, 30, 20); // left eye
+  ellipse(220, 180, 30, 20); // right eye
+  fill(eyeColor);
+  ellipse(180, 183, 20, 20); // left eye
+  ellipse(220, 183, 20, 20); // right eye
+  ellipseMode(CENTER); // left pupil
+  fill(0);
+  ellipse(180, 183, 12, 12);
+  ellipseMode(CENTER); // right pupil
+  fill(0);
+  ellipse(220, 183, 12, 12);
+  fill(73, 20, 3);
+  stroke(100, 0, 0);
+  arc(200, 225, 90, 80, 0, PI); // mouth
+  // teeth
+  fill(225);
+  triangle(165, 225, 170, 265, 175, 225); // left fang
+  triangle(180, 225, 190, 245, 200, 225);
+  triangle(200, 225, 210, 245, 220, 225);
+  triangle(225, 225, 230, 265, 235, 225); // right fang
+  // antena
+  fill(180, 182, 184);
+  line(160, 128, 120, 70); // left
+  triangle(110, 80, 107, 49, 130, 60);
+  line(235, 128, 285, 70); // right
+  triangle(275, 65, 300, 58, 296, 85);
+  // arms
+  fill(209, 113, 117);
+  rotate(radians(-20));
+  ellipse(130, 465, 60, 150); // right
+  rotate(radians(50));
+  ellipse(295, 280, 60, 150); // left
+
+}
+
+//naz monster
+function nazMonster(locX, locY, headColor) {
   noStroke();
 
   //upper body
@@ -134,16 +162,16 @@ function monster(locX, locY, headColor) {
 
 //when a key is pressed the monster will move in a specific direction
 function keyPressed() {
-  if (keyCode == RIGHT_ARROW) {
+  if (keyCode == RIGHT_ARROW) { //moves right
     stepX = stepX + 10;
   }
-  if (keyCode == LEFT_ARROW) {
+  if (keyCode == LEFT_ARROW) { //moves left
     stepX = stepX - 10;
   }
-  if (keyCode == UP_ARROW) {
+  if (keyCode == UP_ARROW) { //moves up
     stepY = stepY - 10;
   }
-  if (keyCode == DOWN_ARROW) {
+  if (keyCode == DOWN_ARROW) { //moves down
     stepY = stepY + 10;
   }
 }
